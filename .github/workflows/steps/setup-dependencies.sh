7d8e13ac04920777a90ed2eff9fbbd5060afa7d6
Diegosd75 <74265984+Diegosd75@users.noreply.github.com>
2025-02-24 15:56:40 -0500

    Update setup-dependencies.sh

--- .github/workflows/steps/setup-dependencies.sh
+++ .github/workflows/steps/setup-dependencies.sh
@@ -1,21 +1,23 @@
-name: Setup Dependencies
+#!/bin/bash
 
-description: Install necessary dependencies
+# Instalar dependencias del sistema
+sudo apt-get update
+sudo apt-get remove -y containerd.io docker.io || true
+sudo apt-get install -y golang jq curl pre-commit git wget unzip python3 python3-pip nodejs npm
 
-runs:
-  using: "composite"
-  steps:
-    - name: Install System Dependencies
-      run: |
-        sudo apt-get update
-        sudo apt-get remove -y containerd.io docker.io || true
-        sudo apt-get install -y golang jq curl pre-commit
-      shell: bash
+# Instalar Docker CE
+curl -fsSL https://get.docker.com | sudo sh
+sudo usermod -aG docker $USER
+echo "Docker instalado correctamente."
 
-    - name: Install Docker CE
-      run: |
-        curl -fsSL https://get.docker.com | sudo sh
-        sudo usermod -aG docker $USER
-        newgrp docker
-        docker --version
-      shell: bash
+docker --version
+
+# Instalar Checkov
+pip3 install checkov
+
+# Instalar Bearer
+curl -sfL https://raw.githubusercontent.com/Bearer/bearer/main/scripts/install.sh | sh
+
+# Verificar instalaciones
+checkov --version
+bearer --version
