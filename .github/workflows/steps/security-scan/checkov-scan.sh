d83cfa3f8a533d95a63806788998b70be50fa0ce
Diegosd75 <74265984+Diegosd75@users.noreply.github.com>
2025-02-24 19:48:44 -0500

    Update checkov-scan.sh

--- .github/workflows/steps/security-scan/checkov-scan.sh
+++ .github/workflows/steps/security-scan/checkov-scan.sh
@@ -3,6 +3,9 @@
 # Crear directorio de resultados si no existe
 mkdir -p results
 
+# Asegurar que no existe una carpeta con el mismo nombre
+rm -rf results/checkov-results.json
+
 # Ejecutar Checkov y forzar la generación del archivo
 checkov --directory . --output json --output-file results/checkov-results.json || echo '{}' > results/checkov-results.json
 
