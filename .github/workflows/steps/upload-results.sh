66808b4fd769f80da48260d35aa0e70522a8d751
Diegosd75 <74265984+Diegosd75@users.noreply.github.com>
2025-02-24 22:46:28 -0500

    Update upload-results.sh

--- .github/workflows/steps/upload-results.sh
+++ .github/workflows/steps/upload-results.sh
@@ -34,7 +34,7 @@ SCAN_TYPES=(
   [gitleaks-report.json]="Gitleaks Scan"
   [trivy-results.json]="Trivy Scan"
   [dependency-check-report.xml]="Dependency Check Scan"
-  [bearer-results.json]="Static Analysis"
+  [bearer-results.json]="Static Check"
 )
 
 # Subir los resultados de los escaneos a DefectDojo
