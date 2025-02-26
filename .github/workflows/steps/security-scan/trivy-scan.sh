287834fcbef8a2b46bc5ead3424b3f50a479503e
diegosd75 <diegoalejo765@gmail.com>
2025-02-25 22:45:48 -0500

    Ajuste variable triby

--- .github/workflows/steps/security-scan/trivy-scan.sh
+++ .github/workflows/steps/security-scan/trivy-scan.sh
@@ -2,6 +2,6 @@
 
 # Ejecutar Trivy directamente en la imagen recién construida
 echo "Escaneando la imagen con Trivy..."
-trivy image --format json -o results/trivy-results.json "$IMAGE_NAME"
+trivy image --format json -o results/trivy-results.json "$1"
 
 echo "Trivy scan completado. Reporte generado en results/trivy-results.json"
